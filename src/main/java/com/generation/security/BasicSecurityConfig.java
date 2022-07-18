2a22df4d0f8246596d481e10ab4419fd2deff4e1
TaisBritoCosta <104440707+TaisBritoCosta@users.noreply.github.com>
2022-07-18 20:34:50 -0300

    Adicionando configuração no security

--- src/main/java/com/generation/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/security/BasicSecurityConfig.java
@@ -40,8 +40,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			// antMatchers("/**").permitAll
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
+			.antMatchers(HttpMethod.GET, "/usuarios/*").permitAll()
 			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
